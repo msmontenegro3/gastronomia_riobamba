6d39c008bdd903ba0fd32301d0380f07621a7ff3
msmontenegro3 <mirta.s.montenegro.l@gmail.com>
2025-04-03 02:06:54 -0500

    Documents created, some styles applied

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,12 @@
 import '../styles/main.scss';
 
+
+/* MENU */
+
+const hamburguerButton = document.querySelector('.header__hamburger');
+const nav = document.querySelector('.header__nav');
+
+hamburguerButton.addEventListener('click', () => {
+    hamburguerButton.classList.toggle('active');
+    nav.classList.toggle('active');
+});
